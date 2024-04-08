44fe6a7abf5df92351f3abe13fccc4157bbfc5e7
cedricdicesare <cedric.dicesare@gmail.com>
2024-04-08 09:48:46 +0200

    Bug correction

--- Scan_nmap_TLS3.py
+++ Scan_nmap_TLS3.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime
 
-# Vérifier les installations des dépendances
+# Importation des dépendances
 try:
     import nmap
 except ImportError:
@@ -20,6 +20,8 @@ except ImportError:
     print("Le module tqdm est requis mais n'est pas installé. Exécutez 'pip install tqdm' pour l'installer.")
     sys.exit(1)
 
+import csv
+
 def check_compliance(tls_version, cipher_suite, cert_validity):
     if tls_version in ["TLSv1.2", "TLSv1.3"]:
         try:
@@ -33,10 +35,11 @@ def check_compliance(tls_version, cipher_suite, cert_validity):
         return "KO"
 
 if len(sys.argv) < 2:
-    print("Usage: python script.py <target>")
+    print("Usage: python script.py <target> [csv filename]")
     sys.exit(1)
 
 target = sys.argv[1]
+csv_filename = sys.argv[2] if len(sys.argv) == 3 else None
 scanner = nmap.PortScanner()
 
 print("Initializing TLS information scan...")
@@ -53,7 +56,6 @@ for host in hosts:
         if 'script' in port_info:
             script_output = port_info['script'].get('ssl-cert', '')
             cert_validity = "N/A"
-            # Parse le script ssl-cert pour extraire la date de fin de validité
             if "Not valid after:" in script_output:
                 start = script_output.find("Not valid after:") + len("Not valid after:")
                 end = script_output.find("T", start)
@@ -71,9 +73,18 @@ for host in hosts:
                     break
 
 progress.close()
+
+# Générer et afficher le tableau
 table = PrettyTable(['Host', 'Port', 'TLS Version', 'Cipher Suite', 'Certificate Validity', 'Compliance'])
-table.align = 'l'
 for row in results:
     table.add_row(row)
 print("\n" + str(table))
+
+# Sauvegarde des résultats dans le fichier CSV si un nom est fourni
+if csv_filename:
+    with open(csv_filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Host', 'Port', 'TLS Version', 'Cipher Suite', 'Certificate Validity', 'Compliance'])
+        writer.writerows(results)
+    print(f"\nResults have been saved to {csv_filename}")
 
